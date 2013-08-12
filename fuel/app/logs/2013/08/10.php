8edea95f33572b8e8a2f31784f8c0b89f99954ac
Alex Hughes <alex.hughes000@gmail.com>
2013-08-12 19:41:53 -0400

    Final

--- fuel/app/logs/2013/08/10.php
+++
@@ -1,8 +0,0 @@
-<?php defined('COREPATH') or exit('No direct script access allowed'); ?>
-
-WARNING - 2013-08-10 23:48:50 --> Fuel\Core\Fuel::init - The configured locale en_US is not installed on your system.
-WARNING - 2013-08-10 23:49:38 --> Fuel\Core\Fuel::init - The configured locale en_US is not installed on your system.
-WARNING - 2013-08-10 23:53:40 --> Fuel\Core\Fuel::init - The configured locale en_US is not installed on your system.
-WARNING - 2013-08-10 23:54:57 --> Fuel\Core\Fuel::init - The configured locale en_US is not installed on your system.
-WARNING - 2013-08-10 23:55:15 --> Fuel\Core\Fuel::init - The configured locale en_US is not installed on your system.
-WARNING - 2013-08-10 23:55:18 --> Fuel\Core\Fuel::init - The configured locale en_US is not installed on your system.
